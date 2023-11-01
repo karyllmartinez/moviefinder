40d7b3e6c19915aa507e2e7f326e37f21a3e574a
Karyll Martinez <karyllmartinez@Zsas-MacBook-Pro.local>
2023-11-01 22:23:52 +0800

    Deployed website

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
-const API_URL = "http://www.omdbapi.com?apikey=7959d0ca";
+const API_URL = "https://www.omdbapi.com?apikey=7959d0ca";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
