66fd727ab2ab099c13779cd16840a8448d7aabbd
Karyll Martinez <karyllmartinez@Zsas-MacBook-Pro.local>
2023-11-01 21:17:41 +0800

    Updates

---
+++ precache-manifest.db2d3409900e2321b878f07945cd2827.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1e78120004dcb44c5f94af2dd558689",
+    "url": "/moviefinder/index.html"
+  },
+  {
+    "revision": "b499db118fde4a2d35e8",
+    "url": "/moviefinder/static/css/main.24b91a61.chunk.css"
+  },
+  {
+    "revision": "350095798f1108b712da",
+    "url": "/moviefinder/static/js/2.f35c33be.chunk.js"
+  },
+  {
+    "revision": "b499db118fde4a2d35e8",
+    "url": "/moviefinder/static/js/main.7ffa51ed.chunk.js"
+  },
+  {
+    "revision": "31126f5698e1765fd403",
+    "url": "/moviefinder/static/js/runtime~main.afef7d54.js"
+  },
+  {
+    "revision": "095eeb8648466cccedd48fd1577605ce",
+    "url": "/moviefinder/static/media/search.095eeb86.svg"
+  }
+]);
